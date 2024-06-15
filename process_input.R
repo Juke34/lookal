58102095c3df3ec01aaa9b7addb0df627a40e88a
Ine Bonthuis <inebont@uio.no>
2024-06-15 17:17:20 +0200

    load_text function and clean_text function

---
+++ process_input.R
@@ -0,0 +1,27 @@
+# File for functions to process the input
+
+setwd("C:/Users/inebo/Documents/PhD/RaukR/lookal")
+
+
+load_text <- function(sentence, pdf = FALSE, pdf_input){
+  if(pdf == TRUE){
+    sentence <- pdftools::pdf_text(pdf_input)
+  }
+  if(length(sentence) > 1){
+    updated_string <- c()
+    for (ii in (1:length(sentence))) {
+      updated_string <- str_c(updated_string, sentence[ii], sep = " ")
+      
+    }
+    sentence <- updated_string
+  }
+  sentence
+}
+
+
+clean_text <- function(sentence){
+  clean_sentence <- str_replace_all(sentence, pattern = "[[:punct:]]|[[:digit:]]", replacement = "")
+  separate_sentence <- str_split_1(clean_sentence, pattern = "[[:space:]]") 
+  sentence_list <- as.list(separate_sentence)
+  sentence_list <- sentence_list[nchar(sentence_list) > 0]
+}
