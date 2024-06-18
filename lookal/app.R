282120afefb5d60d54d493914d6fefbc7484ae0b
Tobias Fietze <tobias.fietze@slu.se>
2024-06-18 13:41:09 +0200

    Implentation of result table

--- lookal/app.R
+++ lookal/app.R
@@ -12,6 +12,9 @@ library(pdftools)
 library(leaflet)
 library(jsonlite)
 library(sf)
+library(DT)
+
+
 
 # Define UI for application
 ui <- fluidPage(
@@ -24,14 +27,14 @@ ui <- fluidPage(
         align-items: center;
         justify-content: center;
         padding: 10px 0;
-        background-color: #f8f9fa; /* Light background for the header */
-        border-bottom: 1px solid #e0e0e0; /* Optional: Add a subtle border below the header */
-        margin-bottom: 20px; /* Space below the header */
+        background-color: #f8f9fa;
+        border-bottom: 1px solid #e0e0e0; 
+        margin-bottom: 20px; 
       }
       
       .logo {
-        height: 50px; /* Adjust the logo size */
-        margin-left: auto; /* Space between the logo and the title */
+        height: 50px;
+        margin-left: auto; 
       }
       
       .center-content {
@@ -57,74 +60,71 @@ ui <- fluidPage(
       
     "))
   ),
-  # Application title
-  #titlePanel("Team Lookal"),
-  
+
   # Header with logo and title
   div(class = "header",
       span(class = "title-text", "Team Lookal"), # Title text
       img(src = "https://nbisweden.github.io/raukr-2024/assets/logos/raukr.png", class = "logo") # Logo image
-      
-  ),
-
-  # Centralized layout for inputs
-  fluidRow(
-    column(12,
-           div(class = "center-content",
-              
-               # Text for project description
-                div(style = "max-width: 600px; margin: auto; text-align: justify;",
-                    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                ),
-           
-               # Select between text and PDF
-               div(style = "width: 200%; max-width: 300px; margin-top: 10px;",
-                   selectInput("input_type", "Choose Input Type:", 
-                               choices = c("Text" = "text", "PDF" = "pdf"),
-                               selected = "text")
-               ),
-               
-               
-              
-               # Text area for text input
-               conditionalPanel(
-                 condition = "input.input_type == 'text'",
-                 div(style = "width: 150%; max-width: 600px; margin-top: 10px;",
-                     textAreaInput("text", "Your text:", "", rows = 3, resize = "vertical", placeholder = "Please enter your text here!")
-                 )
-               ),
-               
-               # File upload for PDF
-               conditionalPanel(
-                 condition = "input.input_type == 'pdf'",
-                 div(style = "width: 80%; max-width: 600px; margin-top: 10px;",
-                     fileInput("pdf", "Upload a PDF file:", accept = c(".pdf"))
-                 )
-               ),
-               
-               # Button to submit the input
-               div(class = "submit-button",
-                   actionButton("submit", "Submit")
-               )
-           )
-    )
-  ),
-
-  # Map panel to display the map
-  fluidRow(
-    column(12,
-           leafletOutput("map", height = "300px") # Adjust the height as needed
-    )
   ),
   
-  # Output panel to display text results below the map
-  fluidRow(
-    column(12,
-           h3("Output:"),
-           textOutput("displayText")
+  # Sidebar layout for potential future navigation or controls
+  sidebarLayout(
+    sidebarPanel(width = 3,
+      # Placeholder for sidebar content
+      p("This is the sidebar content: ....")
+    ),
+    
+    # Main panel for the primary content
+    mainPanel(width= 9,
+      div(style = "max-width: 800px; margin: auto;",
+          # Text for project description
+          div(style = "text-align: justify; margin-bottom: 20px;",
+              p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+          ),
+          
+          # Select between text and PDF
+          div(style = "width: 200px; margin-bottom: 10px;",
+              selectInput("input_type", "Choose Input Type:", 
+                          choices = c("Text" = "text", "PDF" = "pdf"),
+                          selected = "text")
+          ),
+          
+          # Conditional panel for text input
+          conditionalPanel(
+            condition = "input.input_type == 'text'",
+            div(style = "width: 200%; max-width: 800px; margin-bottom: 10px;",
+                textAreaInput("text", "Your text:", "", rows = 3, resize = "vertical", placeholder = "Please enter your text here!")
+            )
+          ),
+          
+          # Conditional panel for PDF upload
+          conditionalPanel(
+            condition = "input.input_type == 'pdf'",
+            div(style = "width: 100%; max-width: 600px; margin-bottom: 10px;",
+                fileInput("pdf", "Upload a PDF file:", accept = c(".pdf"))
+            )
+          ),
+          
+          # Button to submit the input
+          div(class = "submit-button",
+              actionButton("submit", "Submit")
+          ),
+          
+          # Map output
+          leafletOutput("map"),
+          
+          # Text output
+          #h3("Output:"),
+          #textOutput("displayText"),
+          
+          # Data frame output
+          h3("Your text is mainly connected to the following languages:"),
+          DT::dataTableOutput("data_table")  # Placeholder for the data table
+      )
     )
   )
 )
+
 
 
 # Define server logic
@@ -157,11 +157,17 @@ server <- function(input, output) {
   geojson_url <- "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json"
   geojson_data <- st_read(geojson_url) # Read GeoJSON directly from the URL
   
+  
   # Render the map with the GeoJSON data
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       addPolygons(data = geojson_data, fill = TRUE, fillColor = "red", color = "white")
+  })
+  
+  #Render the sample table
+  output$data_table <- DT::renderDataTable({
+    datatable(sample_df, options = list(pageLength = 5, autoWidth = TRUE))
   })
   
 }
