7210567dc8dd8a056b8608a78d948d6a7848bfea
Ine Bonthuis <inebont@uio.no>
2024-06-18 14:44:37 +0200

    cleaned main up more

--- main.R
+++ main.R
@@ -8,24 +8,10 @@ sentence <- "gratis. Encyclopaedia: with, magnificent; % quality!"
 rubbish <- "woordenboek is funny with table and chair"
 
 
-process_text <- load_text(sentence)
-process_text <- clean_text(process_text)
+processed_text <- load_text(sentence)
+processed_text <- clean_text(process_text)
 
-process_text <- set_names(process_text, process_text)
-
-var <- map(process_text, get_countries_by_word)
-var_bin <- list_c(var)
-
-res <- tibble(strings = unlist(var_bin)) %>%
-  count(strings, name = "count") %>%
-  arrange(desc(count)) %>%
-  as.data.frame()
-
-colnames(res) <- c("iso", "count")
-
-# to get the words that were not counted in. not_found string
-var_named <- set_names(var, map_chr(var, "iso"))
-idx <- map(var, function(x) nrow(x) < 1)
-not_found <- names(which(map_lgl(idx, ~ .x)))
+processed_text <- set_names(processed_text, processed_text)
 
 
+further_processed <- process_text(sentence)
