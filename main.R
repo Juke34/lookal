03bc08f2826f8201800ac5231052347744abcfa1
Ine Bonthuis <inebont@uio.no>
2024-06-18 14:41:36 +0200

    Put lines into function process_text, removed it from main

--- main.R
+++ main.R
@@ -3,12 +3,16 @@ library(purrr)
 library(tidyverse)
 library(arrow)
 
+
 sentence <- "gratis. Encyclopaedia: with, magnificent; % quality!"
 rubbish <- "woordenboek is funny with table and chair"
 
 
 process_text <- load_text(sentence)
 process_text <- clean_text(process_text)
+
+process_text <- set_names(process_text, process_text)
+
 var <- map(process_text, get_countries_by_word)
 var_bin <- list_c(var)
 
@@ -18,4 +22,10 @@ res <- tibble(strings = unlist(var_bin)) %>%
   as.data.frame()
 
 colnames(res) <- c("iso", "count")
+
+# to get the words that were not counted in. not_found string
+var_named <- set_names(var, map_chr(var, "iso"))
+idx <- map(var, function(x) nrow(x) < 1)
+not_found <- names(which(map_lgl(idx, ~ .x)))
+
 
