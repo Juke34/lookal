3bade31b2e0100ec62d214dcfd831ca1e2bc01ed
Ine Bonthuis <inebont@uio.no>
2024-06-19 09:34:02 +0200

    Make all letters of input string lowercase

--- process_input.R
+++ process_input.R
@@ -29,10 +29,12 @@ load_text <- function(sentence, pdf = FALSE, pdf_input){
 clean_text <- function(sentence){
   # this function removes all punctuation and digits, to be left with the words.
   # strings are split by space
+  # all letters are converted to lowercase
   # the strings are converted to a list
   # empty character strings are excluded  
   clean_sentence <- str_replace_all(sentence, pattern = "[[:punct:]]|[[:digit:]]", replacement = "")
   separate_sentence <- str_split_1(clean_sentence, pattern = "[[:space:]]") 
+  separate_sentence <- tolower(separate_sentence)
   sentence_list <- as.list(separate_sentence)
   sentence_list <- sentence_list[nchar(sentence_list) > 0]
 }
